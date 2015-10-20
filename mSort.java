3aac18f742f6bd5e053fa43072a43cbb800ec420
Connor <connor.odonnell@cuw.edu>
2015-10-20 02:50:46 -0500

    filled a random array to be sorted

--- mSort.java
+++ mSort.java
@@ -1,57 +1,46 @@
+import java.util.Random;
+
+
 public class mSort
-
 {
-
 	public static void main(String[] args) 
-
-	{
-
-		int[] ar = {7,8,1,3,4,2};
-
+	{		
+		Random r = new Random();
+		int[] ar = new int[10];
+		for(int i = 0; i < ar.length; i++)
+		{
+			ar[i] = r.nextInt(50);
+		}
+		mSort.display(ar);
 		mSort.mergerSort(ar);
-		
-
+		mSort.display(ar);
 	}
 	
 	static void display(int[] ar)
 	{
-		System.out.println("********");
+		System.out.println("*****");
 		for(int i = 0; i < ar.length; i++)
 		{
-			System.out.print
+			System.out.print(ar[i] + " ");
 		}
+		System.out.println("");
+		System.out.println("*****");
 	}
-
-
+	
 	static void mergerSort(int[] ar)
-
 	{
-
 		mSort.mergeSortHelper(ar, 0, ar.length-1);
-		
-
 	}
-
-
+	
 	static void mergeSortHelper(int[] ar, int begin, int end)
-
 	{
-
-		mSort.display(ar, begin, end);
-
 		if(begin != end)
-
 		{
 			int leftBegin = begin;
-
 			int leftEnd = ((end-begin)/2 + begin);
-
 			int rightBegin = ((end-begin)/2 + begin) + 1;
-
 			int rightEnd = end;
-
 			mSort.mergeSortHelper(ar, leftBegin, leftEnd);
-
 			mSort.mergeSortHelper(ar, rightBegin, rightEnd);
 			
 			int[] temp = new int[rightEnd - leftBegin + 1];
@@ -74,11 +63,17 @@ public class mSort
 				{
 					if(ar[currLeft] < ar[currRight])
 					{
+						temp[tempPos] = ar[currLeft];
+						currLeft++;
+					}
+					else
+					{
 						temp[tempPos] = ar[currRight];
-						currRight++;	
+						currRight++;
 					}
 				}
 			}
+			//temp is filled with the values from our 2 sublists in order
 			int tempPos = 0;
 			for(int i = leftBegin; i <= rightEnd; i++)
 			{
@@ -86,24 +81,14 @@ public class mSort
 				tempPos++;
 			}
 		}
-
 	}
-
-
+	
 	static void display(int[] ar, int begin, int end)
-
 	{
-
 		for(int i = begin; i <= end; i++)
-
 		{
-
 			System.out.print(ar[i] + " ");
-
 		}
-
 		System.out.println("");
-
 	}
-
 }
